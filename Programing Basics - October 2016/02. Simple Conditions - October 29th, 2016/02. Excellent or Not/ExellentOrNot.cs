3f26480b65a4891a7554303ae21ffebc85b3b0da
menkachev12 <ivan_menkachev@abv.bg>
2017-01-28 07:32:53 +0200

    Updated 4 codes.

---
+++ Programing Basics - October 2016/02. Simple Conditions - October 29th, 2016/02. Excellent or Not/ExellentOrNot.cs
@@ -0,0 +1,24 @@
+﻿namespace Excellent_or_Not
+{
+    using System;
+
+    public class ExellentOrNot
+    {
+        //// Да се напише конзолна програма, която въвежда оценка(десетично число) и отпечатва “Excellent!”, 
+        //// ако оценката е 5.50 или по-висока, или “Not excellent.” в противен случай.
+
+        public static void Main(string[] args)
+        {
+            var grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else
+            {
+                Console.WriteLine("Not excellent.");
+            }
+        }
+    }
+}
---
+++ Programing Basics - October 2016/02. Simple Conditions - October 29th, 2016/04. Greater Number/GreaterNumber.cs
@@ -0,0 +1,26 @@
+﻿namespace Greater_Number
+{
+    using System;
+
+    public class GreaterNumber
+    {
+        //// Да се напише програма, която въвежда две цели числа и отпечатва по-голямото от двете.
+
+        public static void Main()
+        {
+            Console.WriteLine("Enter two integers");
+
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("greater number: " + num1);
+            }
+            else
+            {
+                Console.WriteLine("greater number: " + num2);
+            }
+        }
+    }
+}
