200ba3523cca40cbbb81f36bf404e21da6420c2c
menkachev12 <ivan_menkachev@abv.bg>
2017-01-31 21:46:46 +0200

    Uploaded "Programming Fundamentals", "Data Types and Variables - Lab 18 January, 2017" folder.

---
+++ Programming Fundamentals - January 2017/01. Data Types and Variables/01. Data Types and Variables - Lab January 18, 2017/05. Special Numbers/SpecialNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace _05.Special_Numbers
+{
+    using System;
+
+    public class SpecialNumbers
+    {
+        //// A number is special when its sum of digits is 5, 7 or 11.
+        //// Write a program to read an integer "n" and for all numbers in the range 1…n 
+        //// to print the number and if it is special or not(True / False).
+
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int num = 1; num <= number; num++)
+            {
+                int sumOfDigits = 0;
+                int digits = num;
+
+                while (digits > 0)
+                {
+                    sumOfDigits += digits % 10; // Remains the last digit then sum it with the next one.
+                    digits = digits / 10; // Remove the last digit.
+                }
+
+                bool special = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+
+                Console.WriteLine("{0} -> {1}", num, special);
+            }
+        }
+    }
+}
