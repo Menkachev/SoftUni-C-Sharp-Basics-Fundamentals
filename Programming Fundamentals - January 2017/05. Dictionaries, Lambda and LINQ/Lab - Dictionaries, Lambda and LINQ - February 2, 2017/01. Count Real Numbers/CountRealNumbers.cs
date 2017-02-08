e825a2cea9dd35c2c9a1e47ab55870bc737b2799
menkachev12 <ivan_menkachev@abv.bg>
2017-02-08 23:07:53 +0200

    Uploaded Lab exercises from Dictionaries, Lambda and LINQ in "Programming Fundamentals".

---
+++ Programming Fundamentals - January 2017/05. Dictionaries, Lambda and LINQ/Lab - Dictionaries, Lambda and LINQ - February 2, 2017/01. Count Real Numbers/CountRealNumbers.cs
@@ -0,0 +1,40 @@
+﻿namespace _01.Count_Real_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountRealNumbers
+    {
+        //// Read a list of real numbers and print them in ascending order 
+        //// along with their number of occurrences.
+
+        //// Example: Input: 8 2.5 2.5 8 2.5 -> Output: 2.5 -> 3, 8 -> 2
+
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(double.Parse);
+
+            var result = new SortedDictionary<double, int>();
+
+            //// The following code counts number of times each simbol/word appear in the input.
+
+            foreach (var number in numbers)
+            {
+                if (!result.ContainsKey(number))
+                {
+                    result[number] = 0;
+                }
+
+                result[number]++;
+            }
+
+            //// Prints numbers key and value on the console.
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - January 2017/05. Dictionaries, Lambda and LINQ/Lab - Dictionaries, Lambda and LINQ - February 2, 2017/03. Min, Max, Sum, Average/MinMaxSumAverage.cs
@@ -0,0 +1,30 @@
+﻿namespace _03.Min__Max__Sum__Average
+{
+    using System;
+    using System.Linq;
+
+    public class MinMaxSumAverage
+    {
+        //// Write a program to read "n" integers and print their sum, min, max, first, last and 
+        //// average values.
+
+        //// Example: Input: 5, 12, 20, -5, 37, 8, -> Output: Sum = 72, Min = -5, Max = 37, Average = 14.4
+
+        public static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            var numbers = new int[num];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Sum = {numbers.Sum()}");
+            Console.WriteLine($"Min = {numbers.Min()}");
+            Console.WriteLine($"Max = {numbers.Max()}");
+            Console.WriteLine($"Average = {numbers.Average()}");
+        }
+    }
+}
