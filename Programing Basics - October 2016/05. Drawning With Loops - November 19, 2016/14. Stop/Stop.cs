fdc72ec6f15b73762c36368b8b5be1b14aca3b1d
menkachev12 <ivan_menkachev@abv.bg>
2017-03-21 01:47:41 +0200

    Uploaded Stop sign exercise.

---
+++ Programing Basics - October 2016/05. Drawning With Loops - November 19, 2016/14. Stop/Stop.cs
@@ -0,0 +1,48 @@
+﻿namespace Stop
+{
+    using System;
+
+    public class Stop
+    {
+        //// Пета задача от изпитa на 26 март 2016. 
+
+        //// Да се напише програма, която прочита от конзолата цяло число "N" и чертае предупредителен знак 
+        //// "STOP" като в примерa по-долу.
+
+        //// Example: Input: 3 -> Output:  ...._______....
+        ////                               ...//_____\\...
+        ////                               ..//_______\\..
+        ////                               .//_________\\.
+        ////                               //___STOP!___\\
+        ////                               \\___________//
+        ////                               .\\_________//.
+        ////                               ..\\_______//..
+
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.Write(new string('.', n + 1));
+            Console.Write(new string('_', (n * 2) + 1));
+            Console.WriteLine(new string('.', n + 1));
+
+            ////Upper part.
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('.', n - i) + "//" + (new string('_', ((n * 2) - 1) + i + i)
+                    + "\\\\" + new string('.', n - i)));
+            }
+
+            //// Middle part.
+            Console.WriteLine("//" + new string('_', (n * 2) - 3) + "STOP!" + new string('_', (n * 2) - 3) + "\\\\");
+            Console.WriteLine("\\\\" + new string('_', (n * 4) - 1) + "//");
+
+            //// Bottom part.
+                for (int i = n - 1; i > 0; i--)
+            {
+                Console.WriteLine(new string('.', n - i) + "\\\\" + (new string('_', ((n * 2) - 1) + i + i)
+                    + "//" + new string('.', n - i)));
+            }
+        }
+    }
+}
