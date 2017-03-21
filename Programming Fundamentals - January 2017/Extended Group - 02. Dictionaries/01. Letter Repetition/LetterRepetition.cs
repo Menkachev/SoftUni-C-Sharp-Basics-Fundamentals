530bb1a6dc94fbc00a826de1d9ce6dfc3ea8350a
menkachev12 <ivan_menkachev@abv.bg>
2017-03-21 22:59:47 +0200

    Uploaded Dictionaries exercises from the extended group.

---
+++ Programming Fundamentals - January 2017/Extended Group - 02. Dictionaries/01. Letter Repetition/LetterRepetition.cs
@@ -0,0 +1,34 @@
+﻿namespace _01.Letter_Repetition
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class LetterRepetition
+    {
+        // You will be given a single string, containing random ASCII character. 
+        // Your task is to print every character, and how many times it has been 
+        // used in the string.
+
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var result = new Dictionary<char, int>();
+
+            foreach (var letter in input)
+            {
+                if (!result.ContainsKey(letter))
+                {
+                    result[letter] = 0;
+                }
+
+                result[letter]++;
+            }
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - January 2017/Extended Group - 02. Dictionaries/06. Filter Base/FilterBase.cs
@@ -0,0 +1,81 @@
+﻿namespace _06.Filter_Base
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class FilterBase
+    {
+        public static void Main(string[] args)
+        {
+            var employesAge = new Dictionary<string, int>();
+            var employesSalary = new Dictionary<string, double>();
+            var employesPosition = new Dictionary<string, string>();
+
+            var input = Console.ReadLine();
+
+            while (input != "filter base")
+            {
+                var tokens = input.Split();
+                var name = tokens[0];
+                var condition = tokens[2];
+
+                employesPosition[name] = condition;
+
+                var ageNumber = 0;
+                if (int.TryParse(condition, out ageNumber))
+                {
+                    employesAge[name] = ageNumber;
+                }
+
+                var salaryNumber = 0.0;
+                if (double.TryParse(condition, out salaryNumber))
+                {
+                    employesSalary[name] = salaryNumber;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var secondInput = Console.ReadLine();
+
+            if (secondInput == "Age")
+            {
+                foreach (var name in employesAge)
+                {
+                    var employee = name.Key;
+                    var age = name.Value;
+
+                    Console.WriteLine($"Name: {employee}");
+                    Console.WriteLine($"Age: {age}");
+                    Console.WriteLine(new string('=', 20));
+                }
+            }
+
+            else if (secondInput == "Salary")
+            {
+                foreach (var name in employesSalary)
+                {
+                    var employee = name.Key;
+                    var salary = name.Value;
+
+                    Console.WriteLine($"Name: {employee}");
+                    Console.WriteLine($"Salary: {salary}");
+                    Console.WriteLine(new string('=', 20));
+                }
+            }
+
+            else if (secondInput == "Position")
+            {
+                foreach (var name in employesPosition)
+                {
+                    var employee = name.Key;
+                    var position = name.Value;
+
+                    Console.WriteLine($"Name: {employee}");
+                    Console.WriteLine($"Position: {position}");
+                    Console.WriteLine(new string('=', 20));
+                }
+            }
+        }
+    }
+}
