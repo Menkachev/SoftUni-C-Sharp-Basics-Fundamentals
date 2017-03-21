d56243d5f466e94aed4db15f4b63529415fc5277
menkachev12 <ivan_menkachev@abv.bg>
2017-03-21 22:37:26 +0200

    Uploaded Dictionaries exercises from the extended group.

---
+++ Programming Fundamentals - January 2017/Extended Group - 02. Dictionaries/Dictionaries - Exercises/02. Dict-Ref/Dict-Ref.cs
@@ -0,0 +1,50 @@
+﻿namespace _02.Dict_Ref
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Dict_Ref
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var result = new Dictionary<string, int>();
+
+            while (input != "end")
+            {
+                var line = input
+                    .Split()
+                    .ToList();
+
+                var name = line[0];
+                var lastElement = line[line.Count - 1];
+
+                var number = 0;
+                if (int.TryParse(lastElement, out number))
+                {
+                    result[name] = number;
+                }
+                else
+                {
+                    if (result.ContainsKey(lastElement))
+                    {
+                        var referenceValue = result[lastElement];
+                        result[name] = referenceValue;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var itemPrice in result)
+            {
+                var name = itemPrice.Key;
+                var price = itemPrice.Value;
+
+                Console.WriteLine($"{name} === {price}");
+            }
+        }
+    }
+}
