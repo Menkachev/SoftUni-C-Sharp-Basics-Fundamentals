5dc87690f01d473acc183e2fd2bf6dfb5bde06d1
menkachev12 <ivan_menkachev@abv.bg>
2017-03-21 22:42:05 +0200

    Uploaded Lambda and LINQ Exercises from the extended group.

---
+++ Programming Fundamentals - January 2017/Extended Group - 04. Lambda and LINQ/01. Lab - Lambda and LINQ/01. Min, Max, Sum, Average/MinMaxSumAverage.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.Min__Max__Sum__Average
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MinMaxSumAverage
+    {
+        // Write a program to read n integers and print their sum, min, max, first, 
+        // last and average values.
+
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentN = int.Parse(Console.ReadLine());
+
+                numbers.Add(currentN);
+            }
+
+            double sum = numbers.Sum();
+            int min = numbers.Min();
+            int max = numbers.Max();
+            double average = numbers.Average();
+
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Min = {min}");
+            Console.WriteLine($"Max = {max}");
+            Console.WriteLine($"Average = {average}");
+        }
+    }
+}
