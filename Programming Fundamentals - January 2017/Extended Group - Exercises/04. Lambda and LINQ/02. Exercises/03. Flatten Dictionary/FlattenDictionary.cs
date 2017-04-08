dc35b9609fa9a0f9aa2f9ed45cbbd2d5b5bef46e
menkachev12 <ivan_menkachev@abv.bg>
2017-04-08 04:51:00 +0300

    Removed all exercises from the Extended Group in one folder.

---
+++ Programming Fundamentals - January 2017/Extended Group - Exercises/04. Lambda and LINQ/02. Exercises/03. Flatten Dictionary/FlattenDictionary.cs
@@ -0,0 +1,77 @@
+﻿namespace _03.Flatten_Dictionary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class FlattenDictionary
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> dictionary = new Dictionary<string, Dictionary<string, string>>();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "end")
+            {
+                string[] inputParams = inputLine
+                    .Split(' ')
+                    .ToArray();
+
+                if (inputParams[0] != "flatten")
+                {
+                    string key = inputParams[0];
+                    string innerKey = inputParams[1];
+                    string innerValue = inputParams[2];
+
+                    if (!dictionary.ContainsKey(key))
+                    {
+                        dictionary[key] = new Dictionary<string, string>();
+                    }
+
+                        dictionary[key][innerKey] = innerValue;
+                }
+                else
+                {
+                    string key = inputParams[1]; // The key is [1], because now "flatten" is [0].
+
+                    dictionary[key] = dictionary[key]
+                        .ToDictionary(x => x.Key + x.Value, x => "flatten"); // Concatenate the key and the value with "+".
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Dictionary<string, Dictionary<string, string>> orderedDictionary = dictionary // Order the key by decending of their length.
+                .OrderByDescending(d => d.Key.Length) 
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var key in orderedDictionary)
+            {
+                Console.WriteLine("{0}", key.Key); // Print the key.
+
+                Dictionary<string, string> orderedInnerDictionary = key.Value 
+                    .Where(x => x.Value != "flatten")
+                    .OrderBy(x => x.Key.Length) // Inner Key ordered by ascending of their length.
+                    .ToDictionary(x => x.Key, x => x.Value);
+
+                Dictionary<string, string> flattenedValues = key.Value
+                    .Where(x => x.Value == "flatten")
+                    .ToDictionary(x => x.Key, x => x.Value);
+
+                int counter = 1;
+                foreach (var innerEntry in orderedInnerDictionary)
+                {
+                    Console.WriteLine("{0}. {1} - {2}", counter, innerEntry.Key, innerEntry.Value);
+                    counter++;
+                }
+
+                foreach (var flatten in flattenedValues)
+                {
+                    Console.WriteLine("{0}. {1}", counter, flatten.Key);
+                    counter++;
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - January 2017/Extended Group - Exercises/04. Lambda and LINQ/02. Exercises/04.  CottageScraper/CottageScrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.CottageScraper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CottageScrapper
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
