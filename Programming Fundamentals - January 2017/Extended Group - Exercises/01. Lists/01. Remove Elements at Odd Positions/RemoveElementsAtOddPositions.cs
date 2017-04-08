8f699558ed2e78ecd3682defe740c06adc64a96a
menkachev12 <ivan_menkachev@abv.bg>
2017-04-08 04:42:34 +0300

    Removed all exercises from the Extended Group in one folder.

---
+++ Programming Fundamentals - January 2017/Extended Group - Exercises/01. Lists/01. Remove Elements at Odd Positions/RemoveElementsAtOddPositions.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.Remove_Elements_at_Odd_Positions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RemoveElementsAtOddPositions
+    {
+        //// Write a program which reads a list of space-separated strings and removes 
+        //// the elements at positions[1…3…5…7…etc.]. After that, print the elements 
+        //// with no delimiter. Note that positions are counted from 1.
+
+        public static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(' ').ToList();
+
+            List<string> evenPosition = new List<string>();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    evenPosition.Add(input[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join("", evenPosition));
+        }
+    }
+}
