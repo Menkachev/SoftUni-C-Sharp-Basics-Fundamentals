78eb3f16240a47968128edb093096063693c906f
menkachev12 <ivan_menkachev@abv.bg>
2017-04-08 04:49:34 +0300

    Removed all exercises from the Extended Group in one folder.

---
+++ Programming Fundamentals - January 2017/Extended Group - Exercises/04. Lambda and LINQ/01. Lab/02. Largest 3 Numbers/LargestThreeNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace _02.Largest_3_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LargestThreeNumbers
+    {
+        // Read a list of real numbers and print largest 3 of them. If less than 3 numbers exit, 
+        // print all of them.
+
+        public static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            var firstThreeLargest = nums
+                .OrderByDescending(n => n)
+                .Take(3);
+
+            Console.WriteLine(string.Join(" ", firstThreeLargest));
+        }
+    }
+}
