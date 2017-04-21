5eb24e29eb1c822a107f0639065728d2bbaf07d2
Ivan Menkachev <ivan_menkachev@abv.bg>
2017-04-22 02:33:27 +0300

    Uploaded Mthods exercises.

---
+++ Programming Fundamentals - January 2017/02. Methods. Debugging and Troubleshooting Code/02. Exercises - Methods, Debugging - Jan 24, 2017/03. English Name of Last Digit/EnglishNameOfLastDigit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.English_Name_of_Last_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Programming Fundamentals - January 2017/02. Methods. Debugging and Troubleshooting Code/02. Exercises - Methods, Debugging - Jan 24, 2017/14. Factorial Trailing Zeroes/FactorialTrailingZeroes.cs
@@ -0,0 +1,56 @@
+﻿namespace _14.Factorial_Trailing_Zeroes
+{
+    using System;
+    using System.Numerics;
+
+    public class FactorialTrailingZeroes
+    {
+        // Create a program that counts the trailing zeroes of the factorial of a given number.
+
+        // Example: Input: 5 -> Output: 1   Comments: 5! = 120 -> One trailing zero 
+
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+
+            do
+            {
+                factorial = factorial * number;
+                number--;
+            }
+            while (number > 1);
+
+            var zeros = CountTrailingZeros(number, factorial);
+
+            Console.WriteLine(zeros);
+        }
+
+        // Trailing zeros.
+        private static int CountTrailingZeros(int number, BigInteger factorial)
+        {
+            var stringFact = factorial.ToString();
+
+            var counter = 0;
+
+            for (int i = 0; i < stringFact.Length; i++)
+            {
+                var currentNum = factorial % 10;
+
+                if (currentNum == 0)
+                {
+                    counter++;
+                }
+                else
+                {
+                    break;
+                }
+
+                factorial /= 10;
+            }
+
+            return counter;
+        }
+    }
+}
