2a588d527c3139abfc2dcf9b5b8f6e023640136a
Ivan Menkachev <ivan_menkachev@abv.bg>
2017-04-23 15:09:06 +0300

    Add Roli the Coder solution.

---
+++ Programming Fundamentals - January 2017/Exam Preparation II/04. Roli The Coder/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace _04.Roli_The_Coder
+{
+    using System;
+    using System.Collections.Generic;
+    public class Event
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public SortedSet<string> Participants { get; set; }
+    }
+}
---
+++ Programming Fundamentals - January 2017/Exam Preparation II/04. Roli The Coder/RoliTheCoder.cs
@@ -0,0 +1,62 @@
+﻿namespace _04.Roli_The_Coder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class RoliTheCoder
+    {
+        public static void Main(string[] args)
+        {
+            List<Event> events = new List<Event>();
+
+            string request = Console.ReadLine();
+
+            while (request != "Time for Code")
+            {
+                List<string> requestEll = request.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                string id = requestEll[0];
+                string name = requestEll[1];
+                List<string> currentParticipants = requestEll.Skip(2).ToList();
+
+                if (!name.StartsWith("#") || (events.Any(e => e.ID == id && e.Name != name.TrimStart('#'))))
+                {
+                    request = Console.ReadLine();
+                    continue;
+                }
+
+                if (events.Any(x => x.ID == id))
+                {
+                    int index = events.FindIndex(x => x.ID == id);
+                    for (int i = 0; i < currentParticipants.Count; i++)
+                    {
+                        events[index].Participants.Add(currentParticipants[i]);
+                    }
+                }
+                else
+                {
+                    events.Add(new Event()
+                    {
+                        ID = id,
+                        Name = name.TrimStart('#'),
+                        Participants = new SortedSet<string>(currentParticipants)
+
+                    });
+                }
+
+                request = Console.ReadLine();
+            }
+
+            foreach (var item in events.OrderByDescending(x => x.Participants.Count).ThenBy(x => x.Name))
+            {
+                Console.WriteLine($"{item.Name} - {item.Participants.Count}");
+                foreach (var participant in item.Participants.OrderBy(x => x))
+                {
+                    Console.WriteLine($"{participant}");
+                }
+            }
+        }
+    }
+}
