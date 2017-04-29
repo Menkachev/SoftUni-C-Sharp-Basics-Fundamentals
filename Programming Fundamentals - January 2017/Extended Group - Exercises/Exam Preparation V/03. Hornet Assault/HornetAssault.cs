db1499887856acd9112757ff298db120d38d6558
Ivan Menkachev <ivan_menkachev@abv.bg>
2017-04-29 21:53:52 +0300

    Add notes in the solution.

--- Programming Fundamentals - January 2017/Extended Group - Exercises/Exam Preparation V/03. Hornet Assault/HornetAssault.cs
+++ Programming Fundamentals - January 2017/Extended Group - Exercises/Exam Preparation V/03. Hornet Assault/HornetAssault.cs
@@ -1,10 +1,7 @@
 ﻿namespace Exercise_3
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class HornetAssault
     {
@@ -21,7 +18,7 @@
                 .ToArray();
 
             var power = hornests.Sum();
-            var currentFirstHornetIndex = 0;
+            var currentFirstHornetIndex = 0; 
 
             for (int i = 0; i < beehives.Length; i++)
             {
@@ -33,7 +30,7 @@
                     if (currentFirstHornetIndex < hornests.Length)
                     {
                         power -= hornests[currentFirstHornetIndex];
-                        currentFirstHornetIndex++;
+                        currentFirstHornetIndex++; // When the first hornet is killed, the next one become first.
                     }
                 }
                 else
@@ -42,14 +39,14 @@
                 }
             }
 
-            if (beehives.Any(b => b > 0))
+            if (beehives.Any(b => b > 0)) // If there is any alive bees.
             {
                 var result = beehives.Where(b => b > 0);
                 Console.WriteLine(string.Join(" ", result));
             }
             else
             {
-                var result = hornests.Skip(currentFirstHornetIndex);
+                var result = hornests.Skip(currentFirstHornetIndex); // Skip the dead hornests.
                 Console.WriteLine(string.Join(" ", result));
             }
         }
